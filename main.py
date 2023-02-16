a3237aab96a9e535e443ce9211b0c7bdfdda1d74
Nate Klump <klumpn@oregonstate.edu>
2023-02-15 23:44:48 -0800

    Reorganizing UploadFileRegion class object, style
    Styling class object. Reorganized so __init__ creates the attr., but create() now moves and sets attribute properties. Removing references to QPainter (deprecated).

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import sys
 import platform
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
 
 # GUI FILE
