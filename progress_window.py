d65b61ca74f6cd644cf70c63eb70302eefffc7c9
Liam Zimmermann <zimmerml@oregonstate.edu>
2023-03-14 01:57:37 -0700

    add status text label to progress window

--- progress_window.py
+++ progress_window.py
@@ -2,8 +2,7 @@ import sys
 import time
 from PyQt5 import QtCore
 from PyQt5.QtCore import QThread, pyqtSignal, QObject, pyqtSlot, Qt
-from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QHBoxLayout, QProgressBar, QVBoxLayout
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QApplication, QPushButton, QWidget, QHBoxLayout, QProgressBar, QVBoxLayout, QLabel
 
 import radiance_pipeline.radiance_pipeline as rp
 from radiance_pipeline.radiance_data import RadianceData
@@ -27,6 +26,10 @@ class ProgressWindow( QWidget ):
         self.progressBar = QProgressBar(self)
         self.progressBar.setGeometry(30, 40, 500, 75)
 
+        self.statusLabel = QLabel("Waiting for Radiance Pipeline to establish"
+                                  " connection", self)
+        self.statusLabel.setGeometry(30, 60, 500, 50)
+
         self.progressValue = 0
 
         self.layout = QVBoxLayout()
@@ -49,21 +52,21 @@ class ProgressWindow( QWidget ):
     def startRadiancePipeline( self, MainWindow ):
         # Radiance data object init.
         radianceDataObject = RadianceData(diameter = MainWindow.diameter,
-                            crop_x_left = MainWindow.crop_x_left,
-                            crop_y_down = MainWindow.crop_y_down,
-                            view_angle_vertical = MainWindow.view_angle_vertical,
-                            view_angle_horizontal = MainWindow.view_angle_horizontal,
-                            target_x_resolution = MainWindow.target_x_resolution,
-                            target_y_resolution = MainWindow.target_y_resolution,
-                            paths_ldr = MainWindow.paths_ldr,
-                            path_temp = MainWindow.path_temp,
-                            path_errors= MainWindow.path_errors,
-                            path_logs= MainWindow.path_logs,
-                            path_rsp_fn = MainWindow.path_rsp_fn,
-                            path_vignetting = MainWindow.path_vignetting,
-                            path_fisheye = MainWindow.path_fisheye,
-                            path_ndfilter = MainWindow.path_ndfilter,
-                            path_calfact = MainWindow.path_calfact)
+                crop_x_left = MainWindow.crop_x_left,
+                crop_y_down = MainWindow.crop_y_down,
+                view_angle_vertical = MainWindow.view_angle_vertical,
+                view_angle_horizontal = MainWindow.view_angle_horizontal,
+                target_x_resolution = MainWindow.target_x_resolution,
+                target_y_resolution = MainWindow.target_y_resolution,
+                paths_ldr = MainWindow.paths_ldr,
+                path_temp = MainWindow.path_temp,
+                path_errors= MainWindow.path_errors,
+                path_logs= MainWindow.path_logs,
+                path_rsp_fn = MainWindow.path_rsp_fn,
+                path_vignetting = MainWindow.path_vignetting,
+                path_fisheye = MainWindow.path_fisheye,
+                path_ndfilter = MainWindow.path_ndfilter,
+                path_calfact = MainWindow.path_calfact)
         
         print( "MainWindow.path_temp: {}".format( MainWindow.path_temp ) )
         
@@ -79,6 +82,7 @@ class ProgressWindow( QWidget ):
             print(rp.radiance_pipeline_get_percent())
             if(rp.radiance_pipeline_get_percent() == 100):
                 progress_bar_update_timer.cancel()
+            self.statusLabel.setText(rp.radiance_pipeline_get_status_text())
 
             
         progress_bar_update_timer = Repeat_Timer(1, update, [self])
