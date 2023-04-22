55f0b41d5aab384264d5b2776f7d36d767f9f988
Nate Klump <klumpn@oregonstate.edu>
2023-04-22 00:41:58 -0700

    Wanderson's code is mostly removed

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-################################################################################
-##
-## BY: WANDERSON M.PIMENTA
-## PROJECT MADE WITH: Qt Designer and PySide2
-## V: 1.0.0
-##
-################################################################################
-
 import sys
 from PySide2.QtWidgets import *
 
