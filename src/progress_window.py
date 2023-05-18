8b69997529e92e0b47231a14d0fe6d6e3dceeea1
Nate Klump <klumpn@oregonstate.edu>
2023-05-18 01:17:06 -0700

    Fixing windows crash bug, add controls, add styling
    Moved watcher update function too QTimer from thread. Adding cancel/finish buttons to queue a cancellation of the active pipeline; finish button to close the window. adjusted layouts. added styling

--- src/progress_window.py
+++ src/progress_window.py
@@ -1,96 +1,196 @@
-import sys
-import time
-from PyQt5 import QtCore
-from PyQt5.QtCore import QThread, pyqtSignal, QObject, pyqtSlot, Qt
-from PySide6.QtWidgets import QApplication, QPushButton, QWidget, QHBoxLayout, QProgressBar, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget, QProgressBar, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+from PySide6.QtCore import QTimer
 
 import submodules.radiance_pipeline.radiance_pipeline as rp
 from submodules.radiance_pipeline.radiance_data import RadianceData
 
 import threading
-from threading import Timer
-import time
-import asyncio
-
-class Repeat_Timer(Timer):
-# https://stackoverflow.com/questions/12435211
-# /threading-timer-repeat-function-every-n-seconds
-    def run(self):
-        while not self.finished.wait(self.interval):
-            self.function(*self.args, **self.kwargs)
 
 
 class ProgressWindow( QWidget ):
-    def __init__( self, MainWindow ):
+    def __init__ (self, MainWindow ):
         QWidget.__init__( self )
 
-        self.progressBar = QProgressBar(self)
+        # Create flags and set default values
+        self.pipelineIsComplete = False
+
+        # Style path
+        self.progress_window_style_path = "./src/styles/progress_window_styles.css"
+
+        # Create widgets
+        self.progressBar = QProgressBar( self )
+        self.progressBar.setObjectName( "progressBar" )
         self.progressBar.setGeometry(30, 40, 500, 75)
 
-        self.statusLabel = QLabel("Waiting for Radiance Pipeline to establish"
-                                  " connection", self)
+        self.statusLabel = QLabel( "Waiting for Radiance Pipeline to establish connection", self )
+        self.statusLabel.setObjectName( "statusLabel" )
         self.statusLabel.setGeometry(30, 60, 500, 50)
 
-        self.progressValue = 0
+        self.cancelButton = QPushButton( "Cancel", self )
+        self.cancelButton.setObjectName( "cancelButton" )
+        self.cancelButton.setToolTip( "Click to cancel the active pipeline. The currently-running step will finish first." )
+        self.cancelButton.clicked.connect( self.cancelPipeline )
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.progressBar)
-        self.setLayout(self.layout)
-        self.setGeometry(300, 300, 550, 100)
-        self.setWindowTitle('HDRI Calibration Pipeline Progress')
-        
+        self.finishButton = QPushButton( "Finish", self )
+        self.finishButton.setObjectName( "finishButton" )
+        self.finishButton.clicked.connect( self.closeWindow )
+
+        # Setup layout
+        self.v_layout = QVBoxLayout()
+        self.v_layout.addWidget( self.progressBar )
+
+        self.h_layout = QHBoxLayout()
+        self.h_layout.addWidget( self.statusLabel, stretch=6 )
+        self.h_layout.addWidget( self.cancelButton, stretch=2 )
+        self.h_layout.addWidget( self.finishButton, stretch=2 )
+
+        self.v_layout.addLayout( self.h_layout )
+
+        self.setLayout( self.v_layout )
+
+        # Setup window properties
+        self.setGeometry( 300, 300, 550, 100 )
+        self.setWindowTitle( 'HDRI Calibration Pipeline Progress' )
+
         self.show()
+
+        self.setWidgetProperties()
 
         # Start the Radiance pipeline
         self.startRadiancePipeline( MainWindow )
 
+        # Create a QTimer for updating the progress regularly
+        self.progressTimer = QTimer( self )
+        self.progressTimer.timeout.connect( self.updateProgress )
+        self.progressTimer.start( 1000 )  # Update every 1 second (1000ms)
 
-    def on_count_changed(self, value):
-        self.progressBar.setValue(value)
+
+    # Cancel button click event: calls cance_pipeline() from radiance_pipeline.py to set a cancel flag.
+    # Queues a cancel on the active pipeline, meaning the currently active step will complete first.
+    def cancelPipeline( self ):
+        self.cancelButton.setText( "Cancelling..." )
+
+        rp.cancel_pipeline()
+
+        self.statusLabel.setText( rp.radiance_pipeline_get_status_text() )
 
         return
 
-    
-    def startRadiancePipeline( self, MainWindow ):
-        # Radiance data object init.
-        radianceDataObject = RadianceData(diameter = MainWindow.diameter,
-                crop_x_left = MainWindow.crop_x_left,
-                crop_y_down = MainWindow.crop_y_down,
-                view_angle_vertical = MainWindow.view_angle_vertical,
-                view_angle_horizontal = MainWindow.view_angle_horizontal,
-                target_x_resolution = MainWindow.target_x_resolution,
-                target_y_resolution = MainWindow.target_y_resolution,
-                paths_ldr = MainWindow.paths_ldr,
-                path_temp = MainWindow.path_temp,
-                path_errors= MainWindow.path_errors,
-                path_logs= MainWindow.path_logs,
-                path_rsp_fn = MainWindow.path_rsp_fn,
-                path_vignetting = MainWindow.path_vignetting,
-                path_fisheye = MainWindow.path_fisheye,
-                path_ndfilter = MainWindow.path_ndfilter,
-                path_calfact = MainWindow.path_calfact)
-        
-        print( "MainWindow.path_temp: {}".format( MainWindow.path_temp ) )
-        
-        print("\n#########\nCALLING PIPELINE COMMAND\n#########\n")
-        t_rp = threading.Thread(target=rp.radiance_pipeline, args=[radianceDataObject])
-        t_rp.start()
-       
-        # SET UP PROGRESS BAR UPDATE TIMER
-        def update(self):
-        # Note: self refers to the progress window, not the timer.
-            self.progressValue = rp.radiance_pipeline_get_percent()
-            self.progressBar.setValue(rp.radiance_pipeline_get_percent())
-            print(rp.radiance_pipeline_get_percent())
-            if( rp.radiance_pipeline_get_percent() >= 100 ):
-                progress_bar_update_timer.cancel()
-            self.statusLabel.setText(rp.radiance_pipeline_get_status_text())
 
-            
-        progress_bar_update_timer = Repeat_Timer(1, update, [self])
-        progress_bar_update_timer.daemon = True # Not entirely necessary since
-                                                # we expect it to exit at
-                                                # percent=100.
-        progress_bar_update_timer.start() 
+    # Checks the progress of the Radiance pipeline. 
+    def updateProgress( self ):
+        currentProgress = rp.radiance_pipeline_get_percent()
+        self.progressBar.setValue( currentProgress )
+        self.statusLabel.setText( rp.radiance_pipeline_get_status_text() )
+
+        if ( (currentProgress >= 100) or (rp.radiance_pipeline_get_finished() == True) ):
+            # Stop timer
+            self.progressTimer.stop()
+
+            # Update styling flag
+            self.pipelineIsComplete = True
+
+            # update styling
+            self.setWidgetProperties()
+
+        return
+
+
+    # Creates the radianceDataObject from the MainWindow variables, passes to the radiance_pipeline.py script, and starts that script in a thread.
+    def startRadiancePipeline( self, MainWindow ):
+        # Radiance data object initialization
+        radianceDataObject = RadianceData( diameter=MainWindow.diameter,
+                                           crop_x_left=MainWindow.crop_x_left,
+                                           crop_y_down=MainWindow.crop_y_down,
+                                           view_angle_vertical=MainWindow.view_angle_vertical,
+                                           view_angle_horizontal=MainWindow.view_angle_horizontal,
+                                           target_x_resolution=MainWindow.target_x_resolution,
+                                           target_y_resolution=MainWindow.target_y_resolution,
+                                           paths_ldr=MainWindow.paths_ldr,
+                                           path_temp=MainWindow.path_temp,
+                                           path_errors=MainWindow.path_errors,
+                                           path_logs=MainWindow.path_logs,
+                                           path_rsp_fn=MainWindow.path_rsp_fn,
+                                           path_vignetting=MainWindow.path_vignetting,
+                                           path_fisheye=MainWindow.path_fisheye,
+                                           path_ndfilter=MainWindow.path_ndfilter,
+                                           path_calfact=MainWindow.path_calfact)
+
+        # Start the radiance_pipeline in a separate thread
+        self.pipeline_thread = threading.Thread( target=rp.radiance_pipeline, args=[radianceDataObject] )
+        self.pipeline_thread.start()
+
+        return
+
+
+    # Closes the ProgressWindow window. Click event for the finishButton.
+    def closeWindow(self):
+        self.close()
+
+
+    # Sets the visibility, states, and styling of the region's widgets
+    def setWidgetProperties( self ):
+        # Set widget visibility
+        self.setWidgetVisibility()
+
+        # Set widget states
+        self.setWidgetStates()
+
+        # Set widget style
+        self.setWidgetStyle()
+
+        return
+    
+
+    # Sets the visibility of widgets based on the region having a file or not
+    def setWidgetVisibility( self ):
+        # Pipline is not done yet
+        if ( self.pipelineIsComplete == False ):
+            self.finishButton.hide()
+
+            self.cancelButton.show()
         
+        else:
+            self.finishButton.show()
+
+            self.cancelButton.hide()
+
+
+        return
+    
+
+    # Sets the states of widgets
+    def setWidgetStates( self ):
+        # Pipline is not done yet
+        if ( self.pipelineIsComplete == False ):
+            self.finishButton.setEnabled( False )
+            self.cancelButton.setEnabled( True )
+
+        else:
+            self.finishButton.setEnabled( True )
+            self.cancelButton.setEnabled( False )
+
+
+        return
+    
+
+    # Sets the style of widgets
+    def setWidgetStyle( self ):
+        # Set property for stylesheet to apply correct style
+        self.statusLabel.setProperty( "pipelineComplete", self.pipelineIsComplete )
+        self.finishButton.setProperty( "pipelineComplete", self.pipelineIsComplete )
+        self.cancelButton.setProperty( "pipelineComplete", self.pipelineIsComplete )
+        self.progressBar.setProperty( "pipelineComplete", self.pipelineIsComplete )
+
+        # Apply style
+        with open( self.progress_window_style_path, "r" ) as stylesheet:
+            self.finishButton.setStyleSheet( stylesheet.read() )
+        with open( self.progress_window_style_path, "r" ) as stylesheet:
+            self.cancelButton.setStyleSheet( stylesheet.read() )
+        with open( self.progress_window_style_path, "r" ) as stylesheet:
+            self.progressBar.setStyleSheet( stylesheet.read() )
+        with open( self.progress_window_style_path, "r" ) as stylesheet:
+            self.statusLabel.setStyleSheet( stylesheet.read() )
+            
+ 
         return
