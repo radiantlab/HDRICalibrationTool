df523a26d454e95225def481feeb3af864db1383
stack461 <stackerror2023@outlook.com>
2023-11-26 10:36:12 -0800

    API handling

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,12 @@
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![path])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn path(name: &str) {
+   println!("{}", name)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
-import React, { useState, ChangeEvent, useRef } from 'react';
+import React, { useState, ChangeEvent, useRef, useEffect } from 'react';
+import { invoke } from '@tauri-apps/api/tauri'
 
 export default function Home() {
     const [images, setImages] = useState<File[]>([]);
     const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    const [path, setPath] = useState('');
   
     const handleImageSelect = (event: ChangeEvent<HTMLInputElement>) => {
       const selectedImages = Array.from(event.target.files as FileList);
@@ -21,7 +24,14 @@ export default function Home() {
       if (fileInputRef.current) {
         fileInputRef.current.click();
       }
+    };
+
+  const handleUploadClick = () => {
+    invoke<string>('path', { name: '../../' })
+      .then(result => setPath(result))
+      .catch(console.error)
   }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>HDRICalibrationTool</h1>
@@ -29,6 +39,7 @@ export default function Home() {
         <h2>Image Upload</h2>
         <input type="file" accept=".jpg, .jpeg" multiple onChange={handleImageSelect} ref={fileInputRef} className="hidden"/>
         <button onClick={handleClick} className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded">Select Files</button>
+        <button onClick={handleUploadClick} className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded">Upload Files</button>
         <div>Image count: {images.length}</div>
         <div className="image-preview flex flex-wrap">
           {images.map((image, index) => (
