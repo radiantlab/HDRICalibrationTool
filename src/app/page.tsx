728ab38c065afe97cc41fdc99878df8df182b2aa
Shanti Morrell <shantimorrell@gmail.com>
2023-11-27 10:44:22 -0800

    Device and asset paths are saved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,20 @@ import React, { useState, ChangeEvent, useRef, useEffect } from "react";
 import { open } from "@tauri-apps/api/dialog";
 import { invoke, convertFileSrc } from "@tauri-apps/api/tauri";
 
+const DEBUG = false;
+
 export default function Home() {
-  // Holds the device file paths selected by the user during the dialog function - to be used by the backend
+  // Holds the file paths for the backend
+  const [devicePaths, setDevicePaths] = useState<any[]>([]);
+
+  // Holds the file paths for the frontend
+  const [assetPaths, setAssetPaths] = useState<any[]>([]);
+
+
+  // Holds the temporary device file paths selected by the user during the dialog function
   let selected: any | any[] = [];
 
-  // Holds the asset paths selected by the user - to be used by the frontend
+  // Holds the temporary asset paths selected by the user during the dialog function
   let assets: any[] = [];
 
   // Open a file dialog window using the tauri api and update the images array with the results
@@ -25,38 +34,47 @@ export default function Home() {
     if (Array.isArray(selected)) {
       assets = selected.map((item: any) => convertFileSrc(item));
       setImages(images.concat(assets));
+      setDevicePaths(devicePaths.concat(selected));
+      setAssetPaths(assetPaths.concat(assets));
     } else if (selected === null) {
       // user cancelled the selection
     } else {
       // user selected a single file
-      assets[0] = convertFileSrc(selected);
+      assets = [convertFileSrc(selected)];
       setImages(images.concat(assets));
+      setDevicePaths(devicePaths.concat([selected]));
+      setAssetPaths(assetPaths.concat(assets));
     }
-    console.log("selected: ", selected);
-    console.log("assets: ", assets);
+    if (DEBUG) {
+      console.log("Dialog function called.");
+      console.log("selected: ", selected);
+      console.log("assets: ", assets);
+    }
   }
 
   const [images, setImages] = useState<File[]>([]);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-
-  // const [path, setPath] = useState("");
-
-  const handleImageSelect = (event: ChangeEvent<HTMLInputElement>) => {
-    const selectedImages = Array.from(event.target.files as FileList);
-    setImages(images.concat(selectedImages));
-  };
 
   const handleImageDelete = (index: number) => {
     const updatedImages = images.slice();
+    const updatedDevicePaths = devicePaths.slice();
+    const updatedAssetPaths = assetPaths.slice();
     updatedImages.splice(index, 1);
+    updatedDevicePaths.splice(index, 1);
+    updatedAssetPaths.splice(index, 1);
     setImages(updatedImages);
+    setDevicePaths(updatedDevicePaths);
+    setAssetPaths(updatedAssetPaths);
   };
 
-  const handleClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
+  if (DEBUG) {
+    useEffect(() => {
+      console.log("Updated devicePaths: ", devicePaths);
+    }, [devicePaths]);
+
+    useEffect(() => {
+      console.log("Updated assetPaths: ", assetPaths);
+    }, [assetPaths]);
+  }
 
   // const handleUploadClick = () => {
   //   invoke<string>("path", { name: "../../" })
@@ -69,26 +87,7 @@ export default function Home() {
       <h1>HDRICalibrationTool</h1>
       <div>
         <h2>Image Upload</h2>
-        <input
-          type="file"
-          accept=".jpg, .jpeg"
-          multiple
-          onChange={handleImageSelect}
-          ref={fileInputRef}
-          className="hidden"
-        />
-        {/* <button
-          onClick={handleClick}
-          className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded"
-        >
-          Select Files
-        </button> */}
-        {/* <button
-          onClick={handleUploadClick}
-          className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded"
-        >
-          Upload Files
-        </button> */}
+
         <button
           onClick={dialog}
           className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded"
