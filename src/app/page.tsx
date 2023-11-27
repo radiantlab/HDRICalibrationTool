328675304389400d578e0b12906f74ab7a26e748
Shanti Morrell <shantimorrell@gmail.com>
2023-11-27 11:20:16 -0800

    FE calls BE pipeline with images user selected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
   // Holds the file paths for the frontend
   const [assetPaths, setAssetPaths] = useState<any[]>([]);
 
-
   // Holds the temporary device file paths selected by the user during the dialog function
   let selected: any | any[] = [];
 
@@ -76,11 +75,50 @@ export default function Home() {
     }, [assetPaths]);
   }
 
-  // const handleUploadClick = () => {
-  //   invoke<string>("path", { name: "../../" })
-  //     .then((result) => setPath(result))
-  //     .catch(console.error);
-  // };
+  // === Define hardcoded data for testing ===
+
+  // Hardcoded radiance and hdrgen paths for testing
+  const fakeRadiancePath = "/usr/local/radiance/bin/";
+  const fakeHdrgenPath = "/usr/local/bin/";
+
+  // Hardcoded camera response function path
+  const fakeResponseFunction =
+    "../examples/inputs/parameters/response_function_files/Response_function.rsp";
+
+  // Hardcoded output path
+  const fakeOutputPath = "../output/";
+
+  // Hardcoded temp path
+  const fakeTempPath = "../tmp/";
+
+  // Hardcoded fisheye diameter and coordinates of square around fisheye view
+  const fakeDiameter = "3612";
+  const fakeXleft = "1019";
+  const fakeYdown = "74";
+
+  // Hardcoded HDR image resolution
+  const fakeXdim = "1000";
+  const fakeYdim = "1000";
+
+  // Calls the BE pipeline function with the input images the user
+  // selected, and hardcoded data for the rest of the inputs
+  const handleGenerateHDRImage = () => {
+    invoke<string>("pipeline", {
+      radiancePath: fakeRadiancePath,
+      hdrgenPath: fakeHdrgenPath,
+      outputPath: fakeOutputPath,
+      tempPath: fakeTempPath,
+      inputImages: devicePaths,
+      responseFunction: fakeResponseFunction,
+      diameter: fakeDiameter,
+      xleft: fakeXleft,
+      ydown: fakeYdown,
+      xdim: fakeXdim,
+      ydim: fakeYdim,
+    })
+      .then((result) => console.log("Success. Result: ", result))
+      .catch(console.error);
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -93,6 +131,12 @@ export default function Home() {
           className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded"
         >
           Select Files
+        </button>
+        <button
+          onClick={handleGenerateHDRImage}
+          className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded"
+        >
+          Generate HDR Image
         </button>
         <div>Image count: {images.length}</div>
         <div className="image-preview flex flex-wrap">
