9d35eabb5b63fecc9fff9846dbbf191a892f4105
Shanti Morrell <shantimorrell@gmail.com>
2024-02-01 08:22:07 -0800

    Added outline to close button, formatting

--- src/app/settings-modal.tsx
+++ src/app/settings-modal.tsx
@@ -1,17 +1,23 @@
-
-
-export default function SettingsModal({ settings, handleChange, toggleModal }: any) {
+export default function SettingsModal({
+  settings,
+  handleChange,
+  toggleModal,
+}: any) {
   return (
     <>
       {/* ----->  Taken from Tailwind CSS example: https://tailwindui.com/components/application-ui/overlays/dialogs */}
-      <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true"></div>
+      <div
+        className="relative z-10"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      ></div>
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">   
-      {/* <-----  */}
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          {/* <-----  */}
           <div className="border border-black shadow-2xl bg-white">
-
             <h1 className="text-4xl text-center mt-10">Settings</h1>
             <div>
               <div className="w-full border-t-2 border-t-gray-900 px-8 pt-6 pb-8 m-10">
@@ -21,7 +27,9 @@ export default function SettingsModal({ settings, handleChange, toggleModal }: a
                 <div>
                   <div className="flex flex-row space-x-5">
                     <div className="mb-4">
-                      <label className="block mb-2">Radiance Path (binary)</label>
+                      <label className="block mb-2">
+                        Radiance Path (binary)
+                      </label>
                       <input
                         name="radiancePath"
                         type="text"
@@ -51,7 +59,9 @@ export default function SettingsModal({ settings, handleChange, toggleModal }: a
                       ></input>
                     </div>
                     <div className="mb-4">
-                      <label className="block mb-2">Temp Path (debug only)</label>
+                      <label className="block mb-2">
+                        Temp Path (debug only)
+                      </label>
                       <input
                         name="tempPath"
                         type="text"
@@ -75,7 +85,13 @@ export default function SettingsModal({ settings, handleChange, toggleModal }: a
                 </div>
               </div>
               <div>
-                <button type="button" onClick={toggleModal}>Close</button>
+                <button
+                  type="button"
+                  className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded"
+                  onClick={toggleModal}
+                >
+                  Close
+                </button>
               </div>
             </div>
           </div>
