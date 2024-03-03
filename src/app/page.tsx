b57fee2b32d5df8a3ca5093c9e3198a85da7465a
Shanti Morrell <shantimorrell@gmail.com>
2024-03-02 16:25:48 -0800

    Added raw image support (without response fnc, only .CR2 ext)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
         filters: [
           {
             name: "Image",
-            extensions: ["jpg", "jpeg", "JPG", "JPEG"],
+            extensions: ["jpg", "jpeg", "JPG", "JPEG", "CR2"],
           },
         ],
       });
