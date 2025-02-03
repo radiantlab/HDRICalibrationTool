caaabb11fcc2a6890018e960653213bd102d24d4
ArtinL <artinlahni@gmail.com>
2025-02-03 15:07:08 -0800

    Added style upgrades to sidebar

--- src/app/settings-page/page.tsx
+++ src/app/settings-page/page.tsx
@@ -2,20 +2,41 @@
 
 import React from "react";
 
-// NOTE: This page is completely non-functional. It is only
-// a placeholder to show what it could look like.
-// to make this work, we need global state management
-
 export default function SettingsPage() {
   return (
     <div className="bg-white text-black grid grid-cols-4 min-h-screen">
       <div className="col-span-1 bg-gray-300">
-        {/* Empty div for the sidebar */}
+        <div className="pt-10 bg-gray-300 fixed left-0 w-1/4 h-full flex flex-col">
+          <nav>
+            <ul>
+              <li className="pt-2 ml-5">
+                <a href="#external_utilities">External Utilities</a>
+              </li>
+              <li className="pt-2 ml-5">
+                <a href="#output_directory">Output Directory</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="flex flex-col pt-24 gap-3 items-center">
+            <button
+              type="button"
+              className="w-max bg-gray-600 hover:bg-gray-500 text-gray-300 font-semibold py-1 px-4 border-gray-400 rounded"
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              className="w-max bg-gray-600 hover:bg-gray-500 text-gray-300 font-semibold py-1 px-4 border-gray-400 rounded"
+            >
+              Close
+            </button>
+          </div>
+        </div>
       </div>
       <main className="col-span-3 p-4">
         <h1 className="text-2xl font-bold mb-5 pt-10">Settings</h1>
         <div>
-          <h2 className="my-6 text-2xl">External Utilities</h2>
+          <h2 id="external_utilities" className="my-6 text-2xl">External Utilities</h2>
           <div className="flex flex-col space-y-5">
             <div className="flex flex-row items-center justify-between">
               <label
@@ -60,7 +81,7 @@ export default function SettingsPage() {
               ></input>
             </div>
           </div>
-          <h2 className="mt-6 mb-2 text-2xl">Output Directory</h2>
+          <h2 id="output_directory" className="mt-6 mb-2 text-2xl">Output Directory</h2>
           <p className="mb-6 italic">
             Select the directory or type the path where the HDR images
             will be saved.
@@ -80,22 +101,6 @@ export default function SettingsPage() {
                 className="placeholder:text-right w-full shadow appearance-none border border-gray-400 rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
               ></input>
             </div>
-          </div>
-          <div className="pt-10">
-            <button
-              type="button"
-              className="bg-gray-300 hover:bg-gray-400 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded"
-            >
-              Close
-            </button>
-            <button
-              type="button"
-              id="save-button"
-              className="bg-gray-300 hover:bg-gray-400 disabled:bg-gray-600 disabled:hover:bg-gray-600 text-gray-700 font-semibold py-1 px-2 border-gray-400 rounded"
-            >
-              Save
-            </button>
-            <div className="pt-2"></div>
           </div>
         </div>
       </main>
