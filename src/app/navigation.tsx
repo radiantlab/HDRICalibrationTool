267dab91a70e7b7508a62e0e10f17f34a02785a0
ArtinL <artinlahni@gmail.com>
2025-03-03 14:12:37 -0800

    Added version info to main page

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -4,17 +4,23 @@ import React from "react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
-import { getName } from "@tauri-apps/api/app";
+import { getName, getTauriVersion, getVersion } from "@tauri-apps/api/app";
 
 export default function Navigation() {
-  const [appName, setAppName] = useState<string>("HDRI Calibration Interface");
   const pathname = usePathname();
 
+  const [appVersion, setAppVersion] = useState<string>("");
+  const [appName, setAppName] = useState<string>("");
+  const [tauriVersion, setTauriVersion] = useState<string>("");
+
   useEffect(() => {
-    // Retrieves app name from Tauri API
+    // Retrieves app name, app version, and tauri version from Tauri API
     async function fetchAppInfo() {
+      setAppVersion(await getVersion());
       setAppName(await getName());
+      setTauriVersion(await getTauriVersion());
     }
+
     fetchAppInfo();
   }, []);
 
@@ -29,12 +35,16 @@ export default function Navigation() {
           />
           <h1 className="text-2xl font-bold">{appName}</h1>
         </div>
+        <div className="text-sm text-gray-600">
+          <div>App Version: {appVersion}</div>
+          <div>Tauri Version: {tauriVersion}</div>
+        </div>
       </div>
       <div id="link-container" className="flex items-center justify-around h-12 ml-8 mr-8 mt-20 border-l border-r border-gray-400">
-        <Link href="/home-page" className={`flex items-center justify-center w-full h-full p-2 cursor-pointer ${pathname === "/home-page" ? "bg-white" : "hover:bg-gray-200"}`}>
+        <Link href="/home-page" className={`flex items-center justify-center w-full h-full p-2 font-bold ${pathname === "/home-page" ? "bg-white cursor-default" : "hover:bg-gray-200 cursor-pointer"}`}>
           Image Configuration
         </Link>
-        <Link href="/settings-page" className={`flex items-center justify-center w-full h-full p-2 cursor-pointer ${pathname === "/settings-page" ? "bg-white" : "hover:bg-gray-200"}`}>
+        <Link href="/settings-page" className={`flex items-center justify-center w-full h-full p-2 font-bold ${pathname === "/settings-page" ? "bg-white cursor-default" : "hover:bg-gray-200 cursor-pointer"}`}>
           Settings
         </Link>
       </div>
