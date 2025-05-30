381033827622d78d10af2062fc455ded71539734
Samuel <crolls@oregonstate.edu>
2025-05-30 16:08:54 -0700

    Added docstrings

--- src-tauri/src/read_header.rs
+++ src-tauri/src/read_header.rs
@@ -1,6 +1,24 @@
+/**
+ * Module for reading header contents from an HDR file.
+ * 
+ * This module provides Tauri commands to read the contents of the header 
+ * in a specified HDR file. It's used by the application to display the evalglare
+ * value in the image viewer.
+ */
 use std::process::Command;
 use std::path::Path;
 
+/**
+ * Tauri command to read a specific key from the header of an HDR file
+ * 
+ * This function retrieves the value in the header of the given HDR file
+ * associated with the given key.
+ * 
+ * @param file_path - The path to the HDR file 
+ * @param radiance_path_string - The path to Radiance binaries
+ * @param key - The key to look for in the header; of the form "KEY="
+ * @returns Result containing the value 
+ */
 #[tauri::command]
 pub fn read_header_value(file_path: String, radiance_path_string: String, key: String) -> Result<String, String> {
     // Convert radiance_path string to path so .join() can be used
@@ -35,6 +53,15 @@ pub fn read_header_value(file_path: String, radiance_path_string: String, key: S
     Err("read_header_value: failed to extract value from key.".into())
 }
 
+/**
+ * Tauri command to read the entirety of the header from an HDR file 
+ * 
+ * This function retrieves the header of the given HDR file
+ * 
+ * @param file_path - The path to the HDR file 
+ * @param radiance_path_string - The path to Radiance binaries
+ * @returns Result containing the header
+ */
 #[tauri::command]
 pub fn read_header(file_path: String, radiance_path_string: String) -> Result<String, String> {
     // Convert radiance_path string to path so .join() can be used
