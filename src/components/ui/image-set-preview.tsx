5402fc4dab744b76fd37868fb776574d39dfcc46
nil <tomeaton76@gmail.com>
2026-05-09 18:05:02 -0700

    unnecessary rerender bug fix

--- src/components/ui/image-set-preview.tsx
+++ src/components/ui/image-set-preview.tsx
@@ -37,29 +37,34 @@ export function ImageSetPreview({
 			<div className="flex w-full">
 				<div className="flex-1 grid grid-flow-col divide-x border-b pl-2">
 					<div className="font-bold text-2xl flex items-center">{name}</div>
-					{Object.entries({
-						Files: files.length,
-						[fileTypes.length > 1 ? "File Types" : "File Type"]:
-							fileTypes.join(", "),
-						"Average File Size": fileStats.then((stats) =>
-							prettyBytes(
-								stats.reduce((acc, stat) => acc + stat.size, 0) / stats.length,
-							),
-						),
-					} satisfies Record<
-						string,
-						string | number | Promise<string | number>
-					>).map(([key, value]) => (
-						<div
-							key={key}
-							className="flex gap-1 items-center text-sm text-muted-foreground px-2"
-						>
-							{key}:
-							<SkeletonSuspended sizePlaceholder={"placeholder"}>
-								{value}
-							</SkeletonSuspended>
-						</div>
-					))}
+					{useMemo(
+						() =>
+							Object.entries({
+								Files: files.length,
+								[fileTypes.length > 1 ? "File Types" : "File Type"]:
+									fileTypes.join(", "),
+								"Average File Size": fileStats.then((stats) =>
+									prettyBytes(
+										stats.reduce((acc, stat) => acc + stat.size, 0) /
+											stats.length,
+									),
+								),
+							} satisfies Record<
+								string,
+								string | number | Promise<string | number>
+							>).map(([key, value]) => (
+								<div
+									key={key}
+									className="flex gap-1 items-center text-sm text-muted-foreground px-2"
+								>
+									{key}:
+									<SkeletonSuspended sizePlaceholder={"placeholder"}>
+										{value}
+									</SkeletonSuspended>
+								</div>
+							)),
+						[fileStats, fileTypes, files.length],
+					)}
 				</div>
 				<Button
 					variant="outline"
