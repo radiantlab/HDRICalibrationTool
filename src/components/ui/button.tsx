3301b4f012ef75aee00baf80460013dd5cf0d7f8
nil <tomeaton76@gmail.com>
2026-05-15 11:00:15 -0700

    made all button triggers render with the type=button prop to make sure they dont trigger forms

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -42,14 +42,14 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, variant, size, asChild = false, ...props }, ref) => {
+	({ className, variant, size, asChild = false, type = "button", ...props }, ref) => {
 		const Comp = asChild ? Slot : "button";
 		return (
 			<Comp
-				type="button"
 				className={cn(buttonVariants({ variant, size, className }))}
 				ref={ref}
 				{...props}
+				type={asChild ? undefined : type}
 			/>
 		);
 	},
--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -9,7 +9,18 @@ const TooltipProvider = TooltipPrimitive.Provider
 
 const Tooltip = TooltipPrimitive.Root
 
-const TooltipTrigger = TooltipPrimitive.Trigger
+const TooltipTrigger = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Trigger>
+>(({ type, asChild, ...props }, ref) => (
+  <TooltipPrimitive.Trigger
+    ref={ref}
+    type={asChild ? undefined : type ?? "button"}
+    asChild={asChild}
+    {...props}
+  />
+))
+TooltipTrigger.displayName = TooltipPrimitive.Trigger.displayName
 
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
